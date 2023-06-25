2f379509d3beb06b9f56f2bdac3eb98c15bade26
Pablo García Fernández <uo276824@uniovi.es>
2023-06-25 11:42:39 +0200

    Update

--- src/test/java/tests/gui/TestPrincipalWindow.java
+++ src/test/java/tests/gui/TestPrincipalWindow.java
@@ -19,6 +19,8 @@ public class TestPrincipalWindow {
 
 	@Test
 	void testInitialization() {
+		// To avoid errors in GitHub Actions because it causes an error if not.
+		System.setProperty("java.awt.headless", "true");
 		PrincipalWindow frame = new PrincipalWindow(puzzle);
 		// If an Exception arrises test will fail
 		assertTrue(true);
